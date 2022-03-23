90398cee13f836f8a8b8d7a696a4290d29637e66
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-03-23 12:03:12 -0300

    Alteraçoes nas validacoes de name e rate

--- middlewares/rateValidation.js
+++ middlewares/rateValidation.js
@@ -2,10 +2,10 @@ const rateValidation = (req, res, next) => {
   const { talk: { rate } } = req.body;
   // radix problema de novo ----> garantir que o parseInt gera um numero na base 10
   // https://stackoverflow.com/questions/6611824/why-do-we-need-to-use-radix-parameter-when-calling-parseint
-  if (parseInt(rate, 10) < 1 || parseInt(rate, 10) > 5) {
+  if (!parseInt(rate, 10) || parseInt(rate, 10) <= 0 || parseInt(rate, 10) > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
-  if (!rate || rate === '') {
+  if (rate === undefined) {
     return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
