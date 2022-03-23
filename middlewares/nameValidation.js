7b2c23a1d54f6b823457de3cdd7300b966c9bd0f
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-03-22 23:59:15 -0300

    Requisito 04 -- cria validaçao do token, age e name

---
+++ middlewares/nameValidation.js
@@ -0,0 +1,12 @@
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+module.exports = nameValidation;
---
+++ middlewares/tokenValidation.js
@@ -0,0 +1,11 @@
+const tokenValidation = (req, res, next) => {
+  // https://www.geeksforgeeks.org/basic-authentication-in-node-js-using-http-header/;
+  const { authorization } = req.headers;
+  if (!authorization || authorization === '') {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
+
+module.exports = tokenValidation;
